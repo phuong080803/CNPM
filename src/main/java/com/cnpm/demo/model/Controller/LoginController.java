fe0e125d0c08f528238916a6e03e5bf321f3be62
phuong080803 <phuong080803@gmail.com>
2024-10-13 23:23:33 +0700

    13/10

--- src/main/java/com/cnpm/demo/model/Controller/LoginController.java
+++ src/main/java/com/cnpm/demo/model/Controller/LoginController.java
@@ -1,36 +1,36 @@
-package com.cnpm.demo.model.Controller;
+    package com.cnpm.demo.model.Controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.security.authentication.AuthenticationManager;
+    import org.springframework.security.core.Authentication;
+    import org.springframework.security.core.context.SecurityContextHolder;
+    import org.springframework.stereotype.Controller;
+    import org.springframework.web.bind.annotation.GetMapping;
+    import org.springframework.web.servlet.ModelAndView;
 
-import com.cnpm.demo.model.Repository.EmployeeRepository;
+    import com.cnpm.demo.model.Repository.EmployeeRepository;
 
-@Controller
-public class LoginController {
-    @Autowired
-    private EmployeeRepository employeeRepository;
+    @Controller
+    public class LoginController {
+        @Autowired
+        private EmployeeRepository employeeRepository;
 
-    @Autowired
-    private AuthenticationManager authenticationManager;
+        @Autowired
+        private AuthenticationManager authenticationManager;
 
-    // Phương thức GET để hiển thị trang login
-    @GetMapping("/login")
-    public ModelAndView showLoginPage() {
-        return new ModelAndView("login"); // trả về trang login (login.html)
+        // Phương thức GET để hiển thị trang login
+        @GetMapping("/login")
+        public ModelAndView showLoginPage() {
+            return new ModelAndView("login"); // trả về trang login (login.html)
+        }
+
+        @GetMapping("/home_page")
+        public ModelAndView showHomePage() {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            String username = authentication.getName();
+            ModelAndView mav = new ModelAndView("home_page");
+            mav.addObject("username", username);
+            return mav;
+        }
     }
-
-    @GetMapping("/home_page")
-    public ModelAndView showHomePage() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String username = authentication.getName();
-        ModelAndView mav = new ModelAndView("home_page");
-        mav.addObject("username", username);
-        return mav;
-    }
-}
 
